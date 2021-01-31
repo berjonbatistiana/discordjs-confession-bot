0a9764905c479a01cdbcc46d3285a0c6de0e5555
Bermond Jon Batistiana <berjondev@gmail.com>
2021-01-31 04:44:27 -0800

    Fixed schema

--- db/schema.sql
+++ db/schema.sql
@@ -9,26 +9,32 @@ CREATE TABLE
 		guild_id VARCHAR(255) NOT NULL UNIQUE,
 		guild_name TEXT NOT NULL,
 		PRIMARY KEY(guild_id)
-  );
-
+);
 
 CREATE TABLE
-	channel_assignments(
+	channel_assignments_table(
 		channel_id TEXT NOT NULL,
 		channel_name TEXT NOT NULL,
 		fn_id INT NOT NULL UNIQUE,
-		guild_id TEXT NOT NULL,
-		PRIMARY KEY (fn_id),
-		FOREIGN KEY(guild_id) REFERENCES guild_table(guild_id)
-    );
+		guild_id VARCHAR(255) NOT NULL,
+		PRIMARY KEY (fn_id)
+		-- FOREIGN KEY(guild_id) REFERENCES guild_table(guild_id)
+);
 
 CREATE TABLE
-	role_function(
+	role_function_table(
 		role_id VARCHAR(255) NOT NULL,
 		role_name TEXT NOT NULL,
 		fn_code INT NOT NULL UNIQUE,
-		guild_id TEXT NOT NULL,
-		PRIMARY KEY(fn_code, role_id),
-		FOREIGN KEY(guild_id) REFERENCES guild_table(guild_id)
-  );
+		guild_id VARCHAR(255) NOT NULL,
+		PRIMARY KEY(fn_code, role_id)
+		-- FOREIGN KEY(guild_id) REFERENCES guild_table(guild_id)
+);
 
+CREATE TABLE
+	confession_count_table(
+        guild_id VARCHAR(255) NOT NULL,
+        confession_count INT NOT NULL,
+        PRIMARY KEY (guild_id)
+        -- FOREIGN KEY(guild_id) REFERENCES guild_table(guild_id)
+);
